6ac9e8a850cfc53adcc5df4b2374b7bc7ec90aba
Dato Lomiashvili <lomiashvili.dato5@gmail.com>
2024-05-07 16:29:22 +0400

    cleanup Images.cs file

--- AdvancedSnake/Images.cs
+++ AdvancedSnake/Images.cs
@@ -1,25 +1,19 @@
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.Reflection;
 using Avalonia.Media.Imaging;
 
 namespace AdvancedSnake
 {
     public static class Images
     {
-        private static readonly string projectDirectory = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName;
-
-        private static string assetsDirectory = Path.Combine(projectDirectory, "Assets");
+        private static readonly string ProjectDirectory = Directory.GetParent(Environment.CurrentDirectory)!.Parent!.Parent!.FullName;
+        private static readonly string AssetsDirectory = Path.Combine(ProjectDirectory, "Assets");
         
-        static Images()
-        {
-        }
-        public static readonly Bitmap Empty = new Bitmap(Path.Combine(assetsDirectory, "Empty.png"));
-        public static readonly Bitmap Body = new Bitmap(Path.Combine(assetsDirectory, "Body.png"));
-        public static readonly Bitmap Head = new Bitmap(Path.Combine(assetsDirectory, "Head.png"));
-        public static readonly Bitmap Food = new Bitmap(Path.Combine(assetsDirectory, "Food.png"));
-        public static readonly Bitmap DeadBody = new Bitmap(Path.Combine(assetsDirectory, "DeadBody.png"));
-        public static readonly Bitmap DeadHead = new Bitmap(Path.Combine(assetsDirectory, "DeadHead.png"));
+        public static readonly Bitmap Empty = new Bitmap(Path.Combine(AssetsDirectory, "Empty.png"));
+        public static readonly Bitmap Body = new Bitmap(Path.Combine(AssetsDirectory, "Body.png"));
+        public static readonly Bitmap Head = new Bitmap(Path.Combine(AssetsDirectory, "Head.png"));
+        public static readonly Bitmap Food = new Bitmap(Path.Combine(AssetsDirectory, "Food.png"));
+        public static readonly Bitmap DeadBody = new Bitmap(Path.Combine(AssetsDirectory, "DeadBody.png"));
+        public static readonly Bitmap DeadHead = new Bitmap(Path.Combine(AssetsDirectory, "DeadHead.png"));
     }
 }
